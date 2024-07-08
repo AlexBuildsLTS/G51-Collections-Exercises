a545b84c701700165a508ceaf34e7a9a8dfd912d
FullStackOS <alex.youssef@live.com>
2024-07-09 00:13:16 +0200

    VCS

---
+++ src/test/java/Apptest.java
@@ -0,0 +1,17 @@
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class Apptest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
